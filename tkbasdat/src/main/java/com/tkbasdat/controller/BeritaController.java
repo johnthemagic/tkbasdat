92b3aba0185e7f95562cf64f50f9ae160ec04023
Yohannes Sakti Panggabean <yoanhans@gmail.com>
2018-05-07 02:35:48 +0700

    adding security, add login form, update design, add screen rating

--- tkbasdat/src/main/java/com/tkbasdat/controller/BeritaController.java
+++ tkbasdat/src/main/java/com/tkbasdat/controller/BeritaController.java
@@ -1,5 +1,24 @@
 package com.tkbasdat.controller;
 
-public class BeritaController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class BeritaController {
+	@RequestMapping("/berita")
+	public String berita(Model model) {
+		model.addAttribute("title","All Berita");
+		return "viewAllBerita";
+	}
+	@RequestMapping("/berita/rating")
+	public String ratingBerita(Model model) {
+		model.addAttribute("title","Rate the News!");
+		return "rating";
+	}
+	@RequestMapping("/berita/comment")
+	public String commentBerita(Model model) {
+		model.addAttribute("title","Comment the News!");
+		return "comment";
+	}
 }
