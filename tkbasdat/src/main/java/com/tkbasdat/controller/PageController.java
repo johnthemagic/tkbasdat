ba0a87f2c2b4adf706b30fcf4253dee5b11c9637
Yohannes Sakti Panggabean <yoanhans@gmail.com>
2018-05-06 18:58:07 +0700

    update page controller, adding method for handle index and login

--- tkbasdat/src/main/java/com/tkbasdat/controller/PageController.java
+++ tkbasdat/src/main/java/com/tkbasdat/controller/PageController.java
@@ -1,5 +1,19 @@
 package com.tkbasdat.controller;
 
-public class PageController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class PageController {
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping("/login")
+	public String login(Model model) {
+		model.addAttribute("title","Login Page");
+		return "login";
+	}
 }
