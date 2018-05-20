bc6effbc9ee98b4863fb44ad336ba62ea76db89c
Yohannes Sakti Panggabean <yoanhans@gmail.com>
2018-05-20 21:49:21 +0700

    comment & rating stage 1

---
+++ tkbasdat/src/main/java/com/tkbasdat/controller/RatingController.java
@@ -0,0 +1,9 @@
+package com.tkbasdat.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class RatingController {
+	
+}
